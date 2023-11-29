fc961e1dc9366389d51ee26ab4bce47b4b45c852
Ihor Chernomorets <oxnardxvi@gmail.com>
2023-11-29 16:47:29 +0100

    Task completed

--- src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
@@ -2,6 +2,22 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class DaysInMonth {
     public void printDays(int year, int month) {
-
+        if(year < 0 || month <= 0 || month > 12) {
+            System.out.println("invalid date");
+        }
+        else {
+            if (month == 2) {
+                boolean isLeadYear = (year % 4 == 0) && (year % 100 != 0 || year % 400 == 0);
+                if(isLeadYear) {
+                    System.out.println("29");
+                }
+                else {
+                    System.out.println("28");
+                }
+            }
+            else {
+                System.out.println(month % 2 == 0 ? "30" : "31");
+            }
+        }
     }
 }
